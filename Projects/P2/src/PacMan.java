25d2daedf9b347ee87e8a410688f931f1807f8d6
Michelle <michellef.rose@gmail.com>
2021-09-25 12:19:22 -0400

    Completed the first function in the PacMan.java class, get_valid_moves, and also added in a helper method to assist with that function

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -14,8 +14,52 @@ public class PacMan{
 		this.myMap = map;
 	}
 
+	//Made helper function for get_valid_moves()
+	private boolean checkLocationValid(Location l) {
+		return map.getLoc(l) == Map.Type.EMPTY || map.getLoc(l) == Map.Type.COOKIE;
+	}
+
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		//All possible locations 
+		left = new Location (this.myLoc.x - 1, this.myLoc.y);
+		right = new Location (this.myLoc.x + 1, this.myLoc.y);
+		down = new Location (this.myLoc.x, this.myLoc.y - 1);
+		up = new Location (this.myLoc.x, this.myLoc.y + 1);
+		diag_left_up = new Location (this.myLoc.x - 1, this.myLoc.y + 1);
+		diag_left_down = new Location (this.myLoc.x - 1, this.myLoc.y - 1);
+		diag_right_up = new Location (this.myLoc.x + 1, this.myLoc.y + 1);
+		diag_right_down = new Location (this.myLoc.x + 1, this.myLoc.y - 1);
+
+		//Store list to be returned here
+		moves = new ArrayList<Location>();
+
+		//Check if each location works 
+		if (checkLocationValid(left)) {
+			moves.add(left);
+		}
+		if (checkLocationValid(right)) {
+			moves.add(right);
+		}
+		if (checkLocationValid(down)) {
+			moves.add(down);
+		}
+		if (checkLocationValid(up)) {
+			moves.add(up);
+		}
+		if (checkLocationValid(diag_left_up)) {
+			moves.add(diag_left_up);
+		}
+		if (checkLocationValid(diag_left_down)) {
+			moves.add(diag_left_down);
+		}
+		if (checkLocationValid(diag_right_up)) {
+			moves.add(diag_right_up);
+		}
+		if (checkLocationValid(diag_right_down)) {
+			moves.add(diag_right_down);
+		}
+
+		return (moves);	
 	}
 
 	public boolean move() {
