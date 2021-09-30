e7fd468c01e5eb884834cd444a304a32697760a3
emily_son <emilydson07@gmail.com>
2021-09-30 17:47:02 -0400

    Created test for pacman move

--- Projects/P2/tests/TestGhostMove.java
+++ Projects/P2/tests/TestGhostMove.java
@@ -5,6 +5,15 @@ import java.io.*;
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
-	
+		NoFrame fram = new NoFrame(); //Creates a new map and tokens
+
+		//Creating players
+		PacMan pacman = frame.addPacMan(new Location(9, 11));
+
+		//Start the game
+		frame.startGame();
+
+		boolean canMove = pacman.move();
+		assertTrue(canMove);
 	}
 }
