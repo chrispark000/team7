f0236b307e3c161d924b781e346b9c7b8425c020
emily_son <emilydson07@gmail.com>
2021-09-30 17:44:58 -0400

    Implemented pacman move() function and created corresponding test

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -2,6 +2,8 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import javax.swing.JComponent;
 
+import java.util.Random;
+
 public class PacMan{
 	String myName;
 	Location myLoc;
@@ -19,7 +21,16 @@ public class PacMan{
 	}
 
 	public boolean move() {
-		return false;
+		ArrayList<Location> moves = this.get_valid_moves();
+		if (moves.isEmpty()) {
+			return false;
+		} else {
+			Random rand = new Random();
+			int randNum = rand.nextInt(moves.size());
+			Location newLoc = new Location(moves.get(randNum).x, moves.get(randNum).y);
+			myLoc = new Location(myLoc.x - newLoc.x, myLoc.y - newLoc.y);
+			return true;
+		}
 	}
 
 	public boolean is_ghost_in_range() { 
