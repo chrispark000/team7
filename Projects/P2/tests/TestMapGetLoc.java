55324e40445c014909ffd5f4073ce71c1de73650
emily_son <emilydson07@gmail.com>
2021-10-07 04:23:05 -0400

    Test for map getLoc

--- Projects/P2/tests/TestMapGetLoc.java
+++ Projects/P2/tests/TestMapGetLoc.java
@@ -6,6 +6,10 @@ import java.io.*;
 public class TestMapGetLoc {
 	
 	public void testMapGetLoc() {
-		return null;
+		Map myMap = new Map(30);
+		PacManComponent pc = new PacManComponent(2, 4, 20);
+		myMap.add("pacman", new Location(2, 4), pc, Map.Type.PACMAN);
+
+		assertEquals(Map.Type.PACMAN, myMap.getLoc(new Location(2, 4)));
 	}
 }
